5bd6d11811d97f4774abb9274a4a8061b5a36657
Sathvik Dhanya <sdhanya@applause.com>
2019-03-24 21:12:43 -0400

    Add challenge txt to code

--- Section_6/if_else.py
+++ Section_6/if_else.py
@@ -1,4 +1,14 @@
 #!/usr/bin/env python
+
+"""
+Write a small program to ask for a name and an age.
+When both values have been entered, check if the person
+is the right age to go on an 18-30 holiday (they must be
+over 18 and under 31).
+If they are, welcome them to the holiday, otherwise print
+a (polite) message refusing them entry.
+"""
+
 name = input("Enter your name: ")
 age = int(input("Enter your age {0}: ".format(name)))
 
