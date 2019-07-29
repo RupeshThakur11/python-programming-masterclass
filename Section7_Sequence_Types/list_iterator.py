7c43e2abf69b77e1e88f88569996ee9b24d98dc0
Sathvik Dhanya <sdhanya@applause.com>
2019-07-28 22:23:18 -0400

    Section7 challenges

---
+++ Section7_Sequence_Types/list_iterator.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python env
+
+"""
+Create a list of items (you may use either strings or numbers in the list),
+then create an iterator using the iter() function.
+
+Use a for loop to loop "n" times, where n is the number of items in your list.
+Each time round the loop, use next() on your list to print the next item.
+
+hint: use the len() function rather than counting the number of items in the list.
+"""
+
+my_list = ['blah', 'hello', 1, 2, 3, 'cat', 123]
+
+my_iterator = iter(my_list)
+
+for i in range(0, len(my_list)):
+    item = next(my_iterator)
+    print(item)
